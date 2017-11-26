ae2aa71a28f6174902fea3702ec78b6002932e90
Zephraph <zephraph@gmail.com>
2017-11-25 21:52:55 -0500

    Add google site verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The VueNYC homepage' }
+      { hid: 'description', name: 'description', content: 'The VueNYC homepage' },
+      { name: 'google-site-verification', content: 'RVWaoC-PG8ywIVJDIt6SguYHf3_h_pixSjk1rYALiC8' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
